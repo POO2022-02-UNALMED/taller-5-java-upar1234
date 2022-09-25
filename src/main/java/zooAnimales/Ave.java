c9865aca82916a93e714056a70fbea892ad6bc78
upar1234 <upar1234@gmail.com>
2022-09-25 14:20:00 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -6,7 +6,7 @@ public class Ave extends Animal{
 	public static int halcones;
 	public static int aguilas;
 	private String colorPlumas;
-	private int cantidadAves;
+	private static int cantidadAves;
 	
 public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
 		super(nombre, edad, habitat, genero);
