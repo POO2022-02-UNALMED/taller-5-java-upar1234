f3612c97b86ccb9d4304161672204948e2469924
upar1234 <upar1234@gmail.com>
2022-09-25 13:54:45 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Mamifero extends Animal {
 	private static ArrayList<Mamifero> listado;
-	private static int caballos;
-	private static int leones;
+	public static int caballos;
+	public static int leones;
 	private boolean pelaje;
 	private int patas;
 	public static int M=listado.size();
